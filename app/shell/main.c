0b8db48121ec1f77b64d2c7f30d49f868d0fb1bf
geison-lira <geisonvlf.dev@gmail.com>
2026-05-21 01:27:49 -0300

    feat: add shell parser to shell prompt loop

--- app/shell/main.c
+++ app/shell/main.c
@@ -1,8 +1,10 @@
 #include <cliutil/prompt.h>
+#include <cliparser/parser.h>
 
 int main(){
     printf("Welcome to the miniature shell.\n");
     while(1){
         shell_prompt();
+        shell_parser();
     }
 }
