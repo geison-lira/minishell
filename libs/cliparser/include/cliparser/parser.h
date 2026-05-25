13dd7ad0f3792070c6bfd32eb53ff413a5ae21ae
geison-lira <geisonvlf.dev@gmail.com>
2026-05-25 17:18:27 -0300

    feat: add ParserState struct to store argc, argv and parser success status

--- libs/cliparser/include/cliparser/parser.h
+++ libs/cliparser/include/cliparser/parser.h
@@ -3,4 +3,10 @@
 #define MAX_INPUT_LENGTH 258
 #define MAX_ARGS_COUNT 65
 
-void shell_parser();
+typedef struct{
+    int argc;
+    char* argv[MAX_ARGS_COUNT];
+    int success;
+}ParserState;
+
+ParserState shell_parser();
--- libs/cliparser/parser.c
+++ libs/cliparser/parser.c
@@ -1,27 +1,28 @@
 #include <cliparser/parser.h>
 
-void shell_parser(){
+ParserState shell_parser(){
     char usr_input[MAX_INPUT_LENGTH];
-    char* usr_argv[MAX_ARGS_COUNT];
-    int usr_argc;
+    ParserState state = {0};
     if(fgets(usr_input, sizeof(usr_input), stdin) == NULL){
         fprintf(stderr, "error: An unexpected error ocurred while reading user input");
-        return;
+        return state;
     }
     if(strchr(usr_input, '\n') == NULL){
         fprintf(stderr, "error: Input exceed maximum supported length of %d characters\n", MAX_INPUT_LENGTH - 2);
         for(int input_flush = '\0'; input_flush != '\n' && input_flush != EOF; input_flush = getchar()){}
-        return;
+        return state;
     }
     usr_input[strcspn(usr_input, "\n")] = '\0';
     char* arg = strtok(usr_input, " ");
-    for(usr_argc = 0; arg != NULL && usr_argc < MAX_ARGS_COUNT - 1; usr_argc++){
-        usr_argv[usr_argc] = arg;
+    for(state.argc = 0; arg != NULL && state.argc < MAX_ARGS_COUNT - 1; state.argc++){
+        state.argv[state.argc] = arg;
         arg = strtok(NULL, " ");
     }
-    usr_argv[usr_argc] = arg;
+    state.argv[state.argc] = arg;
     if(arg != NULL){
         fprintf(stderr, "error: Input exceeds maximum supported number of %d arguments\n", MAX_ARGS_COUNT - 1);
-        return;
+        return state;
     }
+    state.success = 1;
+    return state;
 }
