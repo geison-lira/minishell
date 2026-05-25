9a5dfc84f9291ddb4459c56f418cd1ff83ca4fcc
geison-lira <geisonvlf.dev@gmail.com>
2026-05-25 14:52:20 -0300

    feat: add shell parser tokenizer that reads up to 64 arguments

--- libs/cliparser/include/cliparser/parser.h
+++ libs/cliparser/include/cliparser/parser.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #define MAX_INPUT_LENGTH 258
+#define MAX_INPUT_ARGS 64
 
 void shell_parser();
--- libs/cliparser/parser.c
+++ libs/cliparser/parser.c
@@ -2,10 +2,22 @@
 
 void shell_parser(){
     char usr_input[MAX_INPUT_LENGTH];
+    char* usr_argv[MAX_INPUT_ARGS];
+    int usr_argc;
     if(fgets(usr_input, sizeof(usr_input), stdin) != NULL){
         if(strchr(usr_input, '\n') == NULL){
             printf("error: Input exceed maximum supported length of %d characters\n", MAX_INPUT_LENGTH - 2);
             for(int input_flush = '\0'; input_flush != '\n' && input_flush != EOF; input_flush = getchar()){}
+        }
+        else{
+            char* arg = strtok(usr_input, " ");
+            for(usr_argc = 0; arg != NULL && usr_argc < MAX_INPUT_ARGS; usr_argc++){
+                usr_argv[usr_argc] = arg;
+                arg = strtok(NULL, " ");
+            }
+            if(arg != NULL){
+                printf("error: Input exceeds maximum supported number of %d arguments\n", MAX_INPUT_ARGS);
+            }
         }
     }
 }
