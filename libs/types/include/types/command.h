8710928b6e8895034ce21d4a2f8362077a0d6d03
geison-lira <geisonvlf.dev@gmail.com>
2026-06-13 23:53:05 -0300

    feat: add public Command type

---
+++ libs/types/include/types/command.h
@@ -0,0 +1,15 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#define MAX_INPUT_LENGTH 256
+#define MAX_ARGS_COUNT 64
+
+typedef struct{
+    char* argv[MAX_ARGS_COUNT+1];
+    char* input_file;
+    char* output_file;
+    int argc;
+    int success;
+}Command;
+
+#endif
