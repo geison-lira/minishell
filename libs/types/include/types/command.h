42b952aba23da00bc9535722d0f23cf510159f00
geison-lira <geisonvlf.dev@gmail.com>
2026-06-16 16:19:13 -0300

    fix: adjust input and output file size calculation

--- libs/types/include/types/command.h
+++ libs/types/include/types/command.h
@@ -6,8 +6,8 @@
 
 typedef struct{
     char* argv[MAX_ARGS_COUNT+1];
-    char* input_file[MAX_ARGS_COUNT/2-1];
-    char* output_file[MAX_ARGS_COUNT/2-1];
+    char* input_file[(int)(MAX_ARGS_COUNT/2)];
+    char* output_file[(int)(MAX_ARGS_COUNT/2)];
     int input_flag;
     int output_flag;
     int argc;
